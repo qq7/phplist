f2ce1436290833f5aa9fd6876186a8bf76bbf07c
Alon Swartz <alon@turnkeylinux.org>
2012-08-15 19:27:54 +0300

    update inithook to set domain

--- overlay/usr/lib/inithooks/bin/phplist.py
+++ overlay/usr/lib/inithooks/bin/phplist.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
-"""Set PHPlist admin password and email
+"""Set PHPlist admin password, email and domain
 
 Option:
     --pass=     unless provided, will ask interactively
     --email=    unless provided, will ask interactively
-
+    --domain=   unless provided, will ask interactively
+                DEFAULT=www.example.com
 """
 
 import re
@@ -21,15 +22,18 @@ def usage(s=None):
     print >> sys.stderr, __doc__
     sys.exit(1)
 
+DEFAULT_DOMAIN="www.example.com"
+
 def main():
     try:
         opts, args = getopt.gnu_getopt(sys.argv[1:], "h",
-                                       ['help', 'pass=', 'email='])
+                                       ['help', 'pass=', 'email=', 'domain='])
     except getopt.GetoptError, e:
         usage(e)
 
-    password = ""
     email = ""
+    domain = ""
+    password = ""
     for opt, val in opts:
         if opt in ('-h', '--help'):
             usage()
@@ -37,6 +41,8 @@ def main():
             password = val
         elif opt == '--email':
             email = val
+        elif opt == '--domain':
+            domain = val
 
     if not password:
         d = Dialog('TurnKey Linux - First boot configuration')
@@ -53,9 +59,22 @@ def main():
             "Enter email address for the PHPlist 'admin' account.",
             "admin@example.com")
 
+    if not domain:
+        if 'd' not in locals():
+            d = Dialog('TurnKey Linux - First boot configuration')
+
+        domain = d.get_input(
+            "PHPlist Domain",
+            "Enter the domain to serve PHPlist.",
+            DEFAULT_DOMAIN)
+
+    if domain == "DEFAULT":
+        domain = DEFAULT_DOMAIN
+
     m = MySQL()
     m.execute('UPDATE phplist.admin SET password=\"%s\" WHERE loginname=\"admin\";' % password)
     m.execute('UPDATE phplist.admin SET email=\"%s\" WHERE loginname=\"admin\";' % email)
+    m.execute('UPDATE phplist.config SET value=\"%s\" WHERE item=\"website\";' % domain)
 
 if __name__ == "__main__":
     main()
